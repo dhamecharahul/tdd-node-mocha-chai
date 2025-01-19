10008b4560682a6d4ae5c11696c107f9beb38fed
dhamecharahul <dhamecharahul@gmail.com>
2025-01-19 16:19:36 +0530

    remove skipped test case used at development time

--- test/sum.test.js
+++ test/sum.test.js
@@ -11,47 +11,6 @@ describe('sumNumbersWithSeparator test cases TDD', () => {
     assert.equal(sumFunc(''), 0 );
   });
 
-  xit('should return delemete as ,', () => {
-    assert.equal(sumFunc('1,2,3'), ',\n' );
-  });
-
-  xit('should return delemete if added in input number string as ;', () => {
-    assert.equal(sumFunc('//;\n1;2;3'), ';' );
-  });
-
-  xit('should return number as 1;2;3', () => {
-    assert.equal(sumFunc('//;\n1;2;3'), '1;2;3' );
-  });
-
-  xit('should return delimeter regex with ;', () => {
-    assert.equal(sumFunc('//;\n1;2;3').toString(), new RegExp(`[\\;]`).toString());
-  });
-
-  xit('should return delimeter regex with ; and ,', () => {
-    assert.equal(sumFunc('//;,\n1;2,3').toString(), new RegExp(`[\\;|\\,]`).toString());
-  });
-
-  xit('should return number array with regex of ;', () => {
-    assert.equal(sumFunc('//;\n1;2;3').toString(), ['1','2','3'].toString());
-  });
-
-  xit('should return number array with regex of ;', () => {
-    assert.equal(sumFunc('//;,\n1;2,3').toString(), ['1','2','3'].toString());
-  });
-
-  xit('should convert the string into number and return numbers instead for char', () => {
-    assert.equal(sumFunc('//;\n1;2;3').toString(), [1,2,3].toString());
-  });
-
-
-  xit('should convert check chars and if NAN is found return 0', () => {
-    assert.equal(sumFunc('//;\n@;2;3').toString(), [0,2,3].toString());
-  });
-
-  xit('should get nagative number if exist and store it', () => {
-    assert.equal(sumFunc('//;\n-1;2;3').toString(), [-1].toString());
-  });
-
   it('should throw error for negative numbers', () => {
     assert.throws(() => sumFunc('1,-2,3'), Error, "Negatives numbers not allowed: -2");
   });
@@ -91,16 +50,5 @@ describe('sumNumbersWithSeparator test cases TDD', () => {
   it('should sum large number like 900009', () => {
     assert.equal(sumFunc('1,900009,2'), 900012);
   });
-
-
-
-  
-
-
-
-
-
-
-
 
 });
